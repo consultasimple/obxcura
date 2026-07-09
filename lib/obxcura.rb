585888601dae187009f8a7421a7ddafddff03ccc
Guillermo Moreno <jmemox@gmail.com>
2026-07-09 11:17:21 -0600

    Initial commit

---
+++ lib/obxcura.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "obxcura/version"
+
+module Obxcura
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/obxcura/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Obxcura
+  VERSION = "0.1.0"
+end
